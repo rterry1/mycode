86074c2224531cf5d9819df4e9e2d98a67c25b26
rterry1 <reggie.terry@gmail.com>
2019-07-23 10:07:21 -0400

    last update

---
+++ deleteme.py
@@ -0,0 +1,5 @@
+#1/bin/rm
+#This script calls on the excutable called **rm** (remove), which is
+#installed to the */bin* directory. When this script is run,
+#bash will execute this script with *RM*, which will remove (delete)
+#the script :p
---
+++ iptaker01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+##the line below creates a single string that is passed to print()
+user_input = input("Please enter an IPv4 IP adddress:")
+
+## print() can be givern a series of objects seperated by a comma
+print("You told me the IPv4 address is:" + user_input)
+
+user_input_next = input("Vendor Name: ")
+print(user_input_next)
