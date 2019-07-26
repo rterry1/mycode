1a540b189ee70602aeff6d80e2578b0c6b98950e
rterry1 <reggie.terry@gmail.com>
2019-07-26 13:57:48 -0400

    your commit message

---
+++ excel.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+
+data = [{"PET"}: "cat", {"AGE": 5}, {"PET"}: "dog, {"AGE": 7}]
+
+for pet in data:
+    print(f"The {pet['PET']} is {pet['AGE']}")
---
+++ learning_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+def mathy(x, y):
+    answer = x + y
+    print(answer)
+    return(answer)
+def subby(a):
+    less_than = 100 - a
+    print(less_than)
+
+subby(mathy(10, 15))
